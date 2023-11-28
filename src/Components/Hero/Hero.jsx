da1496ff6b26802e997c03d31b08a9efb49c1872
Sohit Vishwakarma <sohit777ma@gmail.com>
2023-11-28 17:27:29 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,37 @@
+import './Hero.css'
+import arro from '../../assets/arrow_btn.png'
+import play from '../../assets/play_icon.png'
+import pause from '../../assets/pause_icon.png'
+const Hero = ({setPlayStatus,heroData,heroCount,setHeroCount,playStatus}) => {
+  return (
+    <div className='hero'>
+    <div className="hero-text">
+    <p>{heroData.text1}</p>
+    <p>{heroData.text2}</p>
+    </div>
+    <div className="hero-explore">
+    <p>Explore The Features</p>
+    <img src={arro} alt="" />
+    
+    </div>
+   
+    <div className="hero-dit-play">
+    <ul className="hero-dots">
+
+    <li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+    <li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
+    <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
+    </ul>
+    <div className="hero-play">
+    <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause:play} alt="" />
+    <p>See The Video</p>
+    </div>
+    
+    </div>
+    
+    
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+    <div className="nav-logo">Ev-Olution</div>
+    <ul className='nav-menu'>
+    <li>Home</li>
+    <li>Explore</li>
+    <li>Explore</li>
+    <li className='nav-contact'>About</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Navbar
