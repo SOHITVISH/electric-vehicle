7ca9e916268d13cea790e89db1c5a281bf25862d
Sohit Vishwakarma <sohit777ma@gmail.com>
2023-11-29 10:25:42 +0530

    use your content

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,34 +4,51 @@ import play from '../../assets/play_icon.png'
 import pause from '../../assets/pause_icon.png'
 const Hero = ({setPlayStatus,heroData,heroCount,setHeroCount,playStatus}) => {
   return (
-    <div className='hero'>
-    <div className="hero-text">
-    <p>{heroData.text1}</p>
-    <p>{heroData.text2}</p>
-    </div>
-    <div className="hero-explore">
-    <p>Explore The Features</p>
-    <img src={arro} alt="" />
-    
-    </div>
-   
-    <div className="hero-dit-play">
-    <ul className="hero-dots">
+  <>
+  
 
-    <li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
-    <li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
-    <li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
-    </ul>
-    <div className="hero-play">
-    <img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause:play} alt="" />
-    <p>See The Video</p>
-    </div>
-    
-    </div>
-    
-    
-    </div>
+
+
+  
+  
+  
+  
+  
+  </>
   )
 }
 
 export default Hero
+
+
+
+
+
+
+/* <div className='hero'>
+<div className="hero-text">
+<p>{heroData.text1}</p>
+<p>{heroData.text2}</p>
+</div>
+<div className="hero-explore">
+<p>Explore The Features</p>
+<img src={arro} alt="" />
+
+</div>
+
+<div className="hero-dit-play">
+<ul className="hero-dots">
+
+<li onClick={()=>setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
+<li onClick={()=>setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
+<li onClick={()=>setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
+</ul>
+<div className="hero-play">
+<img onClick={()=>setPlayStatus(!playStatus)} src={playStatus?pause:play} alt="" />
+<p>See The Video</p>
+</div>
+
+</div>
+
+
+</div> */
